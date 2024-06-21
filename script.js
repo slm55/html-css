3e56127624943859ebdb7e842aadeabe97f044c2
slm55 <78506891+slm55@users.noreply.github.com>
2024-06-22 01:52:23 +0500

    intial commit

---
+++ script.js
@@ -0,0 +1,116 @@
+const cards = document.querySelectorAll(".card");
+const result = document.getElementsByClassName("result")[0];
+let matched = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
+let trials = 0;
+let results = [];
+
+function flipCard({ target: clickedCard }) {
+  if (cardOne !== clickedCard && !disableDeck) {
+    clickedCard.classList.add("flip");
+    if (!cardOne) {
+      return (cardOne = clickedCard);
+    }
+    cardTwo = clickedCard;
+    disableDeck = true;
+    let cardOneImg = cardOne.querySelector(".back-view img").src,
+      cardTwoImg = cardTwo.querySelector(".back-view img").src;
+    matchCards(cardOneImg, cardTwoImg);
+  }
+}
+
+function matchCards(img1, img2) {
+  if (img1 === img2) {
+    matched++;
+    if (matched == 8) {
+      fire();
+      setTimeout(() => {
+        results.push(trials);
+        trials = 0;
+        updateDashboard();
+        return shuffleCard();
+      }, 3000);
+    }
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = "";
+    return (disableDeck = false);
+  }
+  trials++;
+  document.querySelector(".trials").textContent = "Trials: " + trials;
+
+  setTimeout(() => {
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
+
+  setTimeout(() => {
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = "";
+    disableDeck = false;
+  }, 1200);
+}
+
+function shuffleCard() {
+  matched = 0;
+  disableDeck = false;
+  cardOne = cardTwo = "";
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+  cards.forEach((card, i) => {
+    card.classList.remove("flip");
+    let imgTag = card.querySelector(".back-view img");
+    imgTag.src = `images/img-${arr[i]}.png`;
+    card.addEventListener("click", flipCard);
+  });
+}
+
+result.style.display = "none";
+shuffleCard();
+
+cards.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
+
+function fire() {
+  const end = Date.now() + 5 * 1000;
+
+  const colors = ["#bb0000", "#ffffff"];
+
+  (function frame() {
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: colors,
+    });
+
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: colors,
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+}
+
+function updateDashboard() {
+  document.querySelector(".trials").textContent = "Trials: 0";
+  const dashboard = document.querySelector(".dashboard");
+  results.sort();
+  if (results.length > 5) {
+    results = results.slice(0, 5);
+  }
+  dashboard.innerHTML = "<h3>Best results</h3>";
+  results.forEach((result, index) => {
+    dashboard.innerHTML += `<p>${index + 1}. ${result} trials</p>`;
+  });
+}
